3a5b8ec533ebbc88a668f5aa2553fa3defaabe72
unknown <wallacetashyn@gmail.com>
2022-11-17 19:34:37 -0500

    created a profile page for patients to manage their profile and showed their previous appointments on the index route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,22 @@
 import NavBar from "../components/navbar";
-import { BsSun } from "react-icons/bs";
-import Cards from "../components/cards";
 import { unstable_getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { useRouter } from "next/router";
-import { getPendingAppointments, getUser } from "../utils/apiService";
 import { getSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { AppointmentType } from "../types";
+import CardsContainer from "../components/cards-container";
 
 export default function Home() {
   const router = useRouter();
-  const [cards, setCards] = useState<AppointmentType[]>([]);
   const [userName, setUserName] = useState<string>("User");
 
-  async function getCards() {
+  async function getName() {
     const user: any = await getSession();
     setUserName(user?.firstName);
-
-    const appointmentCards = await getPendingAppointments(user?.userId);
-    console.log(appointmentCards);
-    for (let appointment of appointmentCards) {
-      const doctor = await getUser(appointment.doctorId);
-      appointment.doctorId = doctor?.lastName;
-    }
-    setCards(appointmentCards);
   }
 
   useEffect(() => {
-    getCards();
+    getName();
   }, []);
 
   return (
@@ -38,9 +26,6 @@ export default function Home() {
         <div className="h-100 d-flex justify-content-between align-items-center mt-5">
           <h2>
             Hello {userName}!
-            <span className="ml-3">
-              <BsSun color="orange" />
-            </span>
           </h2>
           <button
             className="btn btn-primary btn-lg"
@@ -50,8 +35,7 @@ export default function Home() {
             Book an appointment
           </button>
         </div>
-        <h5 className="mt-5 text-muted">Active Appointments</h5>
-        <Cards cards={cards} />
+        <CardsContainer />
       </div>
     </div>
   );
