d46ddd378cbe4b3d381fedeef35858e81b27f268
Tashyn Wallace <95532604+tashynw@users.noreply.github.com>
2022-11-21 17:41:24 -0500

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -35,7 +35,7 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        const response = await fetch("http://localhost:3000/api/user/login", {
+        const response = await fetch("https://patient-appointment-app.netlify.app/api/user/login", {
           method: "POST",
           body: JSON.stringify({
             email: credentials?.email,
