de693f6d36a3c58402416bf50dc976b5287aa41b
Tashyn Wallace <95532604+tashynw@users.noreply.github.com>
2022-11-22 15:45:29 -0500

    Update appointmentRequestEmail.ts

--- emails/appointment_request_email/appointmentRequestEmail.ts
+++ emails/appointment_request_email/appointmentRequestEmail.ts
@@ -26,7 +26,7 @@ export async function sendAppointmentRequestEmail(
       fs.readFileSync("./emails/appointment_request_email/email.html", "utf-8")
     );
     const textTemplate = Handlebars.compile(
-      fs.readFileSync("../emails/appointment_request_email/email.txt", "utf-8")
+      fs.readFileSync("./emails/appointment_request_email/email.txt", "utf-8")
     );
 
     const templateData = {
