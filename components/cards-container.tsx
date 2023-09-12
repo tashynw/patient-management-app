e1274e1f85fb3910966d3dfcdb5db3373b53ede2
Tashyn Wallace <wallacetashyn@gmail.com>
2023-09-11 19:55:40 -0500

    displayed appointments in tabs based on the statuses

--- components/cards-container.tsx
+++ components/cards-container.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 import { AppointmentType, UserType } from "../types";
 import Cards from "./cards";
-import { Center, Heading, Text, VStack, useDisclosure } from "@chakra-ui/react";
+import {
+  Center,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
 import ViewAppointmentDrawer from "./drawers/ViewAppointmentDrawer";
 
 interface CardsContainerProps {
@@ -20,76 +31,67 @@ const CardsContainer = (props: CardsContainerProps) => {
     onClose: onViewAppointmentClose,
   } = useDisclosure();
 
-  function AcceptedAppointments() {
-    return props?.acceptedCards?.length ? (
-      <>
-        <Text fontSize="larger" fontWeight="medium" color="gray.600" mt={8}>
-          Accepted Appointments
-        </Text>
-        <Cards
-          cards={props?.acceptedCards}
-          pageSession={props?.pageSession}
-          onViewAppointmentOpen={onViewAppointmentOpen}
-          setSelectedAppointment={setSelectedAppointment}
-        />
-      </>
-    ) : null;
-  }
-
-  function PendingAppointments() {
-    return props?.pendingCards?.length ? (
-      <>
-        <Text fontSize="larger" fontWeight="medium" color="gray.600" mt={8}>
-          Pending Appointments
-        </Text>
-        <Cards
-          cards={props?.pendingCards}
-          pageSession={props?.pageSession}
-          onViewAppointmentOpen={onViewAppointmentOpen}
-          setSelectedAppointment={setSelectedAppointment}
-        />
-      </>
-    ) : null;
-  }
-
-  function RejectedAppointments() {
-    return props?.rejectedCards?.length ? (
-      <>
-        <Text fontSize="larger" fontWeight="medium" color="gray.600" mt={8}>
-          Rejected Appointments
-        </Text>
-        <Cards
-          cards={props?.rejectedCards}
-          pageSession={props?.pageSession}
-          onViewAppointmentOpen={onViewAppointmentOpen}
-          setSelectedAppointment={setSelectedAppointment}
-        />
-      </>
-    ) : null;
-  }
-
   const isEmpty: boolean =
     !props?.pendingCards?.length &&
     !props?.acceptedCards?.length &&
     !props?.rejectedCards?.length;
+  const isAcceptedEmpty = props?.acceptedCards?.length == 0;
+  const isPendingEmpty = props?.pendingCards?.length == 0;
+  const isRejectedEmpty = props?.rejectedCards?.length == 0;
 
   return (
     <>
-      {isEmpty && (
-        <div style={{ marginTop: "100px" }}>
-          <Center>
+      <VStack w="100%" alignItems="flex-start" gap={5}>
+        {isEmpty ? (
+          <Center w="100%" mt={10}>
             <Heading mt={5} size="md" color="gray.600">
               {props?.pageSession?.role == "Patient"
                 ? `No appointments made. Book an appointment now`
                 : `No patient has booked you for an appointment.`}
             </Heading>
           </Center>
-        </div>
-      )}
-      <VStack w="100%" alignItems="flex-start" gap={5}>
-        <AcceptedAppointments />
-        <PendingAppointments />
-        <RejectedAppointments />
+        ) : (
+          <Tabs w="100%" mt={10}>
+            <TabList>
+              {!isAcceptedEmpty && <Tab>Accepted</Tab>}
+              {!isPendingEmpty && <Tab>Pending</Tab>}
+              {!isRejectedEmpty && <Tab>Rejected</Tab>}
+            </TabList>
+
+            <TabPanels>
+              {!isAcceptedEmpty && (
+                <TabPanel mt={8}>
+                  <Cards
+                    cards={props?.acceptedCards}
+                    pageSession={props?.pageSession}
+                    onViewAppointmentOpen={onViewAppointmentOpen}
+                    setSelectedAppointment={setSelectedAppointment}
+                  />
+                </TabPanel>
+              )}
+              {!isPendingEmpty && (
+                <TabPanel mt={8}>
+                  <Cards
+                    cards={props?.pendingCards}
+                    pageSession={props?.pageSession}
+                    onViewAppointmentOpen={onViewAppointmentOpen}
+                    setSelectedAppointment={setSelectedAppointment}
+                  />
+                </TabPanel>
+              )}
+              {!isRejectedEmpty && (
+                <TabPanel mt={8}>
+                  <Cards
+                    cards={props?.rejectedCards}
+                    pageSession={props?.pageSession}
+                    onViewAppointmentOpen={onViewAppointmentOpen}
+                    setSelectedAppointment={setSelectedAppointment}
+                  />
+                </TabPanel>
+              )}
+            </TabPanels>
+          </Tabs>
+        )}
       </VStack>
       <ViewAppointmentDrawer
         isOpen={isViewAppointmentOpen}
